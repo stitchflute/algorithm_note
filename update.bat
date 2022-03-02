aae88f93b38f09b3d3a0f6a9465125839000efb5
unknown <fugang15@mails.ucas.ac.cn>
2022-03-02 15:45:04 +0800

    update `date`

---
+++ update.bat
@@ -0,0 +1,32 @@
+echo cd note
+cd note
+
+echo gitbook build
+call gitbook build
+
+echo cd ..
+call cd ..
+
+echo git add .
+call git add .
+
+echo git commit 
+call git commit -m "update `date` "
+
+echo git push origin master
+call git push origin master
+
+echo xcopy note/_book/* ../stitchflute.github.io/
+call xcopy note/_book/* ../stitchflute.github.io/
+
+echo cd ../stitchflute.github.io
+call cd ../stitchflute.github.io
+
+echo git add .
+call git add .
+
+echo git commit
+call git commit -m "update `date` "
+
+echo git push origin gh-pages
+call git push origin gh-pages
